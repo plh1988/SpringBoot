9065af3e797d4bcb3ca82acafd59ee43fc241b3e
plh1988 <lakmal.pradeep@gmail.com>
2019-04-15 13:06:51 +0530

    test new

---
+++ src/com/pradeep/trees/Node.java
@@ -0,0 +1,49 @@
+package com.pradeep.trees;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Node {
+	private int id;
+	private String name;
+	private List<Node> childrenNodes;
+	
+	public Node(int id,String name) {
+		this.id=id;
+		this.name=name;
+		this.childrenNodes=null;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Node> getChildrenNodes() {
+		return childrenNodes;
+	}
+
+	public void setChildrenNodes(List<Node> childrenNodes) {
+		this.childrenNodes = childrenNodes;
+	}
+	
+	public void addChildNode(Node node) {
+		if(this.childrenNodes == null) {
+			childrenNodes = new ArrayList<Node>();
+			childrenNodes.add(node);
+		}else {
+			childrenNodes.add(node);
+		}
+	}
+}
